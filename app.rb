4a69d22521cb4211eb580857f73def3fd8f1b6a2
George Blackmore <gtblackmore@gmail.com>
2020-03-15 01:35:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,11 +25,11 @@ class App < Sinatra::Base
     when "add"
       (params[:number1].to_i + params[:number2].to_i).to_s
     when "subtract"
-      (params[:number1] - params[:number2]).to_s
+      (params[:number1].to_i - params[:number2].to_i).to_s
     when "multiply"
-      (params[:number1] * params[:number2]).to_s
+      (params[:number1].to_i * params[:number2].to_i).to_s
     when "divide"
-      (params[:number1] / params[:number2]).to_s
+      (params[:number1].to_i / params[:number2].to_i).to_s
     end
   end
   
